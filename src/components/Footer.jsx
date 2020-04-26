c9e2b9a887f35abe02ce96674b23acc7e4613f25
Roshan Kanwar <roshan.18je0696@am.iitism.ac.in>
2020-04-26 20:03:41 +0530

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 function Footer() {
-    const year = new Date().getFullYear();
-    return (
-        <footer>
-            <p>Copyright ⓒ {year}</p>
-        </footer>
-    );
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-function Header(){
-    return (<header>
-                <h1>Keeper</h1>
-            </header>);
-};
+function Header() {
+  return (
+    <header>
+      <h1>Keeper</h1>
+    </header>
+  );
+}
 
 export default Header;
