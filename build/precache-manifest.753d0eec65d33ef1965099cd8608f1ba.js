48aea4eddf96396f73415f6a4c727d89c787d63d
Roshan Kanwar <roshan.18je0696@am.iitism.ac.in>
2020-04-26 19:58:06 +0530

    updated

---
+++ build/precache-manifest.753d0eec65d33ef1965099cd8608f1ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "232f8d87e4365f3c158b566a84318d06",
+    "url": "/keeperClone/index.html"
+  },
+  {
+    "revision": "18970c6d079f64b0d6ef",
+    "url": "/keeperClone/static/js/2.626dbf51.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/keeperClone/static/js/2.626dbf51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d71803b5d1585f72558",
+    "url": "/keeperClone/static/js/main.21a63d61.chunk.js"
+  },
+  {
+    "revision": "24f06216c26646c3d31b",
+    "url": "/keeperClone/static/js/runtime-main.c9e9a624.js"
+  }
+]);
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 function Note(props) {
-    return (
-        <div className="note">
-            <h1>{props.title}</h1>
-            <p>{props.content}</p>
-            <button>DELETE</button>
-        </div>
-    );
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}>DELETE</button>
+    </div>
+  );
 }
 
 export default Note;
